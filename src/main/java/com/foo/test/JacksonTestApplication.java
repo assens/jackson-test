ea5dc22c79a3dff6ab2e763ae565305676eb4cec
Assen Sharlandjiev <assen.sharlandjiev@gmail.com>
2019-12-26 14:53:59 +0200

    Added @EnableWebFlux

--- src/main/java/com/foo/test/JacksonTestApplication.java
+++ src/main/java/com/foo/test/JacksonTestApplication.java
@@ -2,8 +2,10 @@ package com.foo.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class JacksonTestApplication {
 
   public static void main(String[] args) {
